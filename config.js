c5435db4270726e5d661cb4bc963f56eb92ba5ff
TSCDigital <tramitesyservicioscalakmul@gmail.com>
2026-07-25 17:09:42 -0600

    Add files via upload

---
+++ config.js
@@ -0,0 +1,9 @@
+window.MENU_CONFIG = {
+  restaurantName: "Mariscos y Carnes La Oficina",
+  logo: "logo.png",
+  schedule: "Martes a domingo · 12:00 p. m. a 10:00 p. m.",
+  whatsapp: "529837525905",
+  phone: "9838716462",
+  facebook: "https://www.facebook.com/laoficinamariscosycarnes",
+  maps: "https://maps.app.goo.gl/GNJBnjqYLDsaX2wh9"
+};
